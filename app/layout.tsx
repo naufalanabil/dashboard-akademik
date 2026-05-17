0539f21db6f6633b3b4875810e64903a88fe5df6
naufalanabil <naufalanabil73@gmail.com>
2026-05-17 19:50:07 +0700

    fix layout children type error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,21 @@ import Sidebar from '@/components/Sidebar'
 import './globals.css'
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
+import type { ReactNode } from 'react'
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
-
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
   return (
     <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className="flex">
         {/* Sidebar akan muncul di kiri terus-menerus */}
-        <Sidebar /> 
-        
+        <Sidebar />
+
         <main className="flex-1 ml-[250px] bg-slate-100 min-h-screen">
           {children}
         </main>
